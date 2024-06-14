9da90e2d109039ff741904c9e6f4bde169759f6f
Patrick Schaldenbrand <patrickschaldenbrand@gmail.com>
2024-06-14 15:47:14 -0400

    o7

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 rm -r build/*
 CC=x86_64-unknown-linux-gnu-gcc GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -o build/main main.go
 cp -r views build/
-cp -r static build/static
-cp db build/db.db
+cp -r static build/
+cp db.db build/db.db
 zip -r build.zip build 
